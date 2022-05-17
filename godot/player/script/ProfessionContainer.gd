a44e8de4e0be6d49734aab2d1196480bfde14b0f
oscaralonsob <oscaralonsob@gmail.com>
2022-05-17 10:47:22 +0200

    feat: removed resources that can be created in the class like stats and perks

--- godot/player/script/ProfessionContainer.gd
+++ godot/player/script/ProfessionContainer.gd
@@ -5,6 +5,9 @@ class_name ProfessionContainer
 export (PackedScene) var weapon_scene
 export (Resource) var health
 export (Resource) var speed
+export (Resource) var damage
+export (Resource) var cooldown_reduction
+
 
 func get_health() -> Stat:
     return health as Stat
@@ -12,3 +15,11 @@ func get_health() -> Stat:
 
 func get_speed() -> Stat:
     return speed as Stat
+
+
+func get_damage() -> Stat:
+    return damage as Stat
+
+
+func get_cooldown_reduction() -> Stat:
+    return cooldown_reduction as Stat
