2cd602ffc317d30cdf8ed1c91819329c924a899c
oscaralonsob <oscaralonsob@gmail.com>
2022-05-20 00:00:16 +0200

    feat: first perks added

--- godot/player/script/ProfessionContainer.gd
+++ godot/player/script/ProfessionContainer.gd
@@ -7,6 +7,10 @@ export (Resource) var health
 export (Resource) var speed
 export (Resource) var damage
 export (Resource) var cooldown_reduction
+export (Array, Resource) var perks
+
+
+var _rng = RandomNumberGenerator.new()
 
 
 func get_health() -> Stat:
@@ -23,3 +27,12 @@ func get_damage() -> Stat:
 
 func get_cooldown_reduction() -> Stat:
     return cooldown_reduction as Stat
+
+
+func get_perks() -> Array:
+    return perks
+
+
+func get_random_perk() -> Resource:
+    _rng.randomize()
+    return perks[_rng.randf_range(0, perks.size() - 1)]
